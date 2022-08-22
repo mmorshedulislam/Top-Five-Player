e202d38d13de9a73ea758967870288e2310c7218
MORSHED-HOME <morshed952640@gmail.com>
2022-08-22 19:55:23 +0600

    footer added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,71 @@
+// disable selected player
+const selectedPlayers = document.getElementsByClassName("selected");
+for (const selectedPlayer of selectedPlayers) {
+  selectedPlayer.addEventListener("click", function (event) {
+    selectedPlayer.setAttribute("disabled", true);
+  });
+}
+
+// player select
+const totalPlayers = document.getElementById("total-players");
+function playerSelect(playerId) {
+  const playerName = document.createElement("li");
+  playerName.classList.add("text-white-50");
+  playerName.classList.add("my-3");
+  const playerNameValue = getInnerText(playerId);
+  playerName.innerText = playerNameValue;
+  totalPlayers.appendChild(playerName);
+
+  const totalPlayersCount =
+    document.getElementById("total-players").childElementCount;
+  if (totalPlayersCount > 5) {
+    alert("You have selceted already 5 Players");
+    totalPlayers.removeChild(playerName);
+  }
+}
+
+// player 1
+document.getElementById("selected-1").addEventListener("click", function () {
+  playerSelect("player-name-1");
+});
+
+// player 2
+document.getElementById("selected-2").addEventListener("click", function () {
+  playerSelect("player-name-2");
+});
+
+// player 3
+document.getElementById("selected-3").addEventListener("click", function () {
+  playerSelect("player-name-3");
+});
+
+// player 4
+document.getElementById("selected-4").addEventListener("click", function () {
+  playerSelect("player-name-4");
+});
+
+// player 5
+document.getElementById("selected-5").addEventListener("click", function () {
+  playerSelect("player-name-5");
+});
+
+// player 6
+document.getElementById("selected-6").addEventListener("click", function () {
+  playerSelect("player-name-6");
+});
+
 // Player cost
 document.getElementById("calculate").addEventListener("click", function () {
   const perPlayerCost = getInputValueById("per-player-cost");
-  const numberOfPlayer = 5;
-  const totalPlayerCost = perPlayerCost * numberOfPlayer;
+  const totalPlayersCount =
+    document.getElementById("total-players").childElementCount;
+  const totalPlayerCost = perPlayerCost * totalPlayersCount;
   setInnerTextById("total-expenses", totalPlayerCost);
+  if (isNaN(perPlayerCost) || perPlayerCost === "") {
+    alert("Please Input Valid Number");
+    setInnerTextById("total-expenses", "00");
+    setInputValueById("per-player-cost", "");
+  }
 });
 
 // Total cost
@@ -15,71 +77,13 @@ document
     const totalPlayerCost = getInnerTextById("total-expenses");
     const totalCost = managerCost + coachCost + totalPlayerCost;
     setInnerTextById("in-total", totalCost);
+
+    if (isNaN(managerCost) || isNaN(coachCost)) {
+      alert("Please Input Valid Number");
+      setInnerTextById("in-total", "00");
+      setInputValueById("manager-cost", "");
+      setInputValueById("coach-cost", "");
+    }
   });
 
-// disable selected player
-const selectedPlayers = document.getElementsByClassName("selected");
-for (const selectedPlayer of selectedPlayers) {
-  selectedPlayer.addEventListener("click", function (event) {
-    event.stopImmediatePropagation;
-    selectedPlayer.setAttribute("disabled", true);
-  });
-}
-
-const totalPlayers = document.getElementById("total-players");
-if (totalPlayers.childElementCount >= 5) {
-  alert("you have already selected 5");
-}
-document.getElementById("selected-1").addEventListener("click", function () {
-  const playerName = document.createElement("li");
-  playerName.classList.add("text-white-50");
-  playerName.classList.add("my-3");
-  const playerName1 = getInnerText("player-name-1");
-  playerName.innerText = playerName1;
-  totalPlayers.appendChild(playerName);
-});
-
-document.getElementById("selected-2").addEventListener("click", function () {
-  const playerName = document.createElement("li");
-  playerName.classList.add("text-white-50");
-  playerName.classList.add("my-3");
-  const playerName2 = getInnerText("player-name-2");
-  playerName.innerText = playerName2;
-  totalPlayers.appendChild(playerName);
-});
-
-document.getElementById("selected-3").addEventListener("click", function () {
-  const playerName = document.createElement("li");
-  playerName.classList.add("text-white-50");
-  playerName.classList.add("my-3");
-  const playerName3 = getInnerText("player-name-3");
-  playerName.innerText = playerName3;
-  totalPlayers.appendChild(playerName);
-});
-
-document.getElementById("selected-4").addEventListener("click", function () {
-  const playerName = document.createElement("li");
-  playerName.classList.add("text-white-50");
-  playerName.classList.add("my-3");
-  const playerName4 = getInnerText("player-name-4");
-  playerName.innerText = playerName4;
-  totalPlayers.appendChild(playerName);
-});
-
-document.getElementById("selected-5").addEventListener("click", function () {
-  const playerName2 = document.createElement("li");
-  playerName2.classList.add("text-white-50");
-  playerName2.classList.add("my-3");
-  const playerName5 = getInnerText("player-name-5");
-  playerName2.innerText = playerName5;
-  totalPlayers.appendChild(playerName2);
-});
-
-document.getElementById("selected-6").addEventListener("click", function () {
-  const playerName2 = document.createElement("li");
-  playerName2.classList.add("text-white-50");
-  playerName2.classList.add("my-3");
-  const playerName6 = getInnerText("player-name-6");
-  playerName2.innerText = playerName6;
-  totalPlayers.appendChild(playerName2);
-});
+// validation
--- js/utilities.js
+++ js/utilities.js
@@ -1,24 +1,29 @@
+// get input value
 function getInputValueById(inputId) {
   const inputField = document.getElementById(inputId).value;
   const inputValue = parseInt(inputField);
   return inputValue;
 }
 
+// get innerText with number
 function getInnerTextById(elementId) {
   const element = document.getElementById(elementId).innerText;
   const elementValue = parseInt(element);
   return elementValue;
 }
 
+// get innerText
 function getInnerText(elementId) {
   const element = document.getElementById(elementId).innerText;
   return element;
 }
 
+// set input value
 function setInputValueById(inputId, result) {
   const inputField = (document.getElementById(inputId).value = result);
 }
 
+// set innerText
 function setInnerTextById(elementId, result) {
   const element = (document.getElementById(elementId).innerText = result);
 }
